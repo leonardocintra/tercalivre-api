ad944587e3810127b1d72e16c6d973c2b609964d
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-16 18:37:32 -0200

    Initial Project

---
+++ tercalivre/src/main/java/com/tercalivre/api/TercalivreApplication.java
@@ -0,0 +1,12 @@
+package com.tercalivre.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TercalivreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TercalivreApplication.class, args);
+	}
+}
---
+++ tercalivre/src/test/java/com/tercalivre/api/TercalivreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tercalivre.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TercalivreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
