352541483d174e675e339f622fe6ee666ea31c95
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-17 12:41:03 -0200

    teste

--- tercalivre/src/main/java/com/tercalivre/api/TercalivreApplication.java
+++ tercalivre/src/main/java/com/tercalivre/api/TercalivreApplication.java
@@ -2,9 +2,19 @@ package com.tercalivre.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class TercalivreApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TercalivreApplication.class, args);
