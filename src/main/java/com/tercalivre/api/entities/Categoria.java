05ada296b272f556a9c88e6366a1621752b03e40
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-23 18:48:57 -0200

    removendo anotacoes

--- src/main/java/com/tercalivre/api/entities/Categoria.java
+++ src/main/java/com/tercalivre/api/entities/Categoria.java
@@ -2,10 +2,7 @@ package com.tercalivre.api.entities;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -16,11 +13,8 @@ public class Categoria implements Serializable {
 	private static final long serialVersionUID = 5168881074248158870L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, nullable = false)
 	private Long id;
 
-	@Column(name = "descricao", nullable = false)
 	private String descricao;
 
 	public Long getId() {
