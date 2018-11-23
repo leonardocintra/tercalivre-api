d77cd2c28e6dac483f42eae374938a1e222eeb78
Leonardo Cintra <leonardo.ncintra@outlook.com>
2018-11-23 13:01:16 -0200

    tentando fazer heroku funcionar 34

--- src/main/java/com/tercalivre/api/entities/Autor.java
+++ src/main/java/com/tercalivre/api/entities/Autor.java
@@ -2,7 +2,10 @@ package com.tercalivre.api.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,8 +16,11 @@ public class Autor implements Serializable {
 	private static final long serialVersionUID = 4149136805886139545L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
 	private Long id;
 
+	@Column(name = "nome", nullable = false)
 	private String nome;
 
 	public Long getId() {
